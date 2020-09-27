8f396c1299f1ff8b330b20f151240efa824849cd
dev-93 <cjb940103@gmail.com>
2020-09-27 14:59:29 +0900

    firebaseconfig 수정

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,13 @@
 import * as firebase from "firebase/app";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyA1yXSkC7LFun2pglS3jWqKygkVCZuXWTE",
-    authDomain: "new-twitter-41b2a.firebaseapp.com",
-    databaseURL: "https://new-twitter-41b2a.firebaseio.com",
-    projectId: "new-twitter-41b2a",
-    storageBucket: "new-twitter-41b2a.appspot.com",
-    messagingSenderId: "624993899829",
-    appId: "1:624993899829:web:c60370aa9090005495287c",
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_DATABASE_URL,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGIN_ID,
+    appId: process.env.REACT_APP_APP_ID,
 };
 
 export default firebase.initializeApp(firebaseConfig);
