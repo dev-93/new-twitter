2a5dba415e16e51db8113a424a5b9109f1c5a989
dev-93 <cjb940103@gmail.com>
2020-09-27 14:50:47 +0900

    firebase add 후 import

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import * as firebase from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyA1yXSkC7LFun2pglS3jWqKygkVCZuXWTE",
+    authDomain: "new-twitter-41b2a.firebaseapp.com",
+    databaseURL: "https://new-twitter-41b2a.firebaseio.com",
+    projectId: "new-twitter-41b2a",
+    storageBucket: "new-twitter-41b2a.appspot.com",
+    messagingSenderId: "624993899829",
+    appId: "1:624993899829:web:c60370aa9090005495287c",
+};
+
+export default firebase.initializeApp(firebaseConfig);
