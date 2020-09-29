00761db628166acde4c24656d5fe509d02e27225
dev-93 <cr000@naver.com>
2020-09-30 01:53:09 +0900

    newAccount 기본 true값으로 설정하여 create Account 적용

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -4,7 +4,7 @@ import { authService } from "fbase";
 const Auth = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
-    const [newAccount, setNewAccount] = useState(false);
+    const [newAccount, setNewAccount] = useState(true);
     const [error, setError] = useState("");
     const onChange = (e) => {
         const {
