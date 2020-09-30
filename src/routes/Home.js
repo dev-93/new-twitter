4d83e234e3ee385cb31dd65c6b05d1ab981f004d
dev-93 <cr000@naver.com>
2020-09-30 13:13:38 +0900

    home 수정

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,5 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Home = () => <span>Home</span>;
+const Home = () => {
+    const [nweet, setNweet] = useState("");
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+    };
+
+    const onChange = (e) => {
+        const {
+            target: { value },
+        } = e;
+        setNweet(value);
+    };
+    return (
+        <>
+            <form onSubmit={onSubmit}>
+                <input
+                    type="text"
+                    placeholder="what's on your mind?"
+                    maxLength={120}
+                    onChange={onChange}
+                    value={nweet}
+                />
+                <input type="submit" value="new-twitter" />
+            </form>
+        </>
+    );
+};
 
 export default Home;
