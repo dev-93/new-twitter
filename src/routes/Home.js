a6347be096cf2fb66f59bfcff5d30782237b30e0
dev-93 <cr000@naver.com>
2020-09-30 14:15:00 +0900

    db에 있는 data를 불러와서 렌더링 시키기

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,8 +1,23 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { dbService } from "fbase";
 
 const Home = () => {
     const [text, setText] = useState("");
+    const [data, setData] = useState([]);
+    const getData = async () => {
+        const dbData = await dbService.collection("information").get();
+        dbData.forEach((document) => {
+            const dataObj = {
+                ...document.data(),
+                id: document.id,
+            };
+            setData((prev) => [dataObj, ...prev]);
+        });
+    };
+
+    useEffect(() => {
+        getData();
+    }, []);
 
     const onSubmit = async (e) => {
         e.preventDefault();
@@ -33,6 +48,11 @@ const Home = () => {
                 />
                 <input type="submit" value="submit" />
             </form>
+            <div>
+                {data.map((list) => (
+                    <div key={list.id}>{list.text}</div>
+                ))}
+            </div>
         </>
     );
 };
