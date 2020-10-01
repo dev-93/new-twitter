aff1a4d4e32c9614f226dd3680f3fc71de886909
dev-93 <cr000@naver.com>
2020-10-01 14:48:30 +0900

    delete 클릭 이후, confirm창이 나오고 확인을 누르면 db에서도 화면과 서버에서 deleted

--- src/components/TextList.js
+++ src/components/TextList.js
@@ -1,12 +1,19 @@
+import { dbService } from "fbase";
 import React from "react";
 
 const TextList = ({ listObj, isOwner }) => {
+    const onDelete = async () => {
+        const yes = window.confirm("지우시겠습니까?");
+        if (yes) {
+            await dbService.doc(`new-twitter/${listObj.id}`).delete();
+        }
+    };
     return (
         <div>
             <h4>{listObj.text}</h4>
             {isOwner && (
                 <>
-                    <button>Delete</button>
+                    <button onClick={onDelete}>Delete</button>
                     <button>Edit</button>
                 </>
             )}
