72fafa5b79ac5fb0d3f41e0840dfe8b0ba052001
dev-93 <cr000@naver.com>
2020-10-17 02:16:29 +0900

    Updates

---
+++ precache-manifest.89af4945b1d1005d5cd7f2d966f32d0d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a331810b6ec1099d891da94ebb44da04",
+    "url": "/index.html"
+  },
+  {
+    "revision": "de46ca4216a17dbf31ea",
+    "url": "/static/css/main.907f6507.chunk.css"
+  },
+  {
+    "revision": "097ce6084cf682035044",
+    "url": "/static/js/2.e0943397.chunk.js"
+  },
+  {
+    "revision": "7e320732ce8e269e8d356159c55ade0e",
+    "url": "/static/js/2.e0943397.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de46ca4216a17dbf31ea",
+    "url": "/static/js/main.aa89b284.chunk.js"
+  },
+  {
+    "revision": "cf839f7d6a8ccd21a5db",
+    "url": "/static/js/runtime-main.c70acead.js"
+  }
+]);
